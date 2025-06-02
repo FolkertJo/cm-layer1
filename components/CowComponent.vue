5b59fc0360d1c541fad0b41682e1a8ffc5453b04
Folkert Jongbloed <f.jongbloed@cowmanager.com>
2025-06-02 15:32:57 +0200

    Added component

---
+++ components/CowComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Dashboard</h1>
+  <div class="grid grid-cols-3 gap-2">
+    <div>Panel</div>
+    <div>Panel</div>
+    <div>Panel</div>
+    <div>Panel</div>
+    <div>Panel</div>
+    <div>Panel</div>
+  </div>
+</template>
