88961ae3760abbf7be648f73efedc8da26c12c30
Folkert Jongbloed <f.jongbloed@cowmanager.com>
2025-06-02 15:43:02 +0200

    added padding

--- components/CowComponent.vue
+++ components/CowComponent.vue
@@ -1,11 +1,11 @@
 <template>
   <h1>Dashboard</h1>
   <div class="grid grid-cols-3 gap-2">
-    <div class="rounded-lg bg-gray-100">Panel</div>
-    <div class="rounded-lg bg-gray-100">Panel</div>
-    <div class="rounded-lg bg-gray-100">Panel</div>
-    <div class="rounded-lg bg-gray-100">Panel</div>
-    <div class="rounded-lg bg-gray-100">Panel</div>
-    <div class="rounded-lg bg-gray-100">Panel</div>
+    <div class="rounded-lg bg-gray-100 p-5">Panel</div>
+    <div class="rounded-lg bg-gray-100 p-5">Panel</div>
+    <div class="rounded-lg bg-gray-100 p-5">Panel</div>
+    <div class="rounded-lg bg-gray-100 p-5">Panel</div>
+    <div class="rounded-lg bg-gray-100 p-5">Panel</div>
+    <div class="rounded-lg bg-gray-100 p-5">Panel</div>
   </div>
 </template>
