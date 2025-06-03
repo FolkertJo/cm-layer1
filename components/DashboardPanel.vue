5da0112c73f800a57cdb239de417288ae341b03c
Folkert Jongbloed <f.jongbloed@cowmanager.com>
2025-06-03 13:20:15 +0200

    Replaces dashboard panel content
    Replaces the original dashboard panel content with a new "Services" section.
    
    The new section includes descriptions of services like DAPP Development, Web 3.0 Development, Project Audit, and more, each with a title, description, and visual styling.

--- components/DashboardPanel.vue
+++ components/DashboardPanel.vue
@@ -1,16 +1,77 @@
 <template>
-  <div class="flex flex-col items-center gap-6 p-7  rounded-2xl">
-    <div>
-      <div class="rounded-lg bg-green-100 p-5">Panel</div>
-    </div>
-    <div class="flex items-center">
-      <span class="text-2xl font-medium">Class Warfare</span>
-      <span class="font-medium text-sky-500">The Anti-Patterns</span>
-      <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
-        <span>No. 4</span>
-        <span>·</span>
-        <span>2025</span>
-      </span>
+  <div class="container relative flex flex-col justify-between h-full max-w-6xl px-10 mx-auto xl:px-0 mt-5">
+    <h2 class="mb-1 text-3xl font-extrabold leading-tight text-gray-900">Services</h2>
+    <p class="mb-12 text-lg text-gray-500">Here is a few of the awesome Services we provide.</p>
+    <div class="w-full">
+      <div class="flex flex-col w-full mb-10 sm:flex-row">
+        <div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+          <div class="relative h-full ml-0 mr-0 sm:mr-10">
+            <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-indigo-500 rounded-lg"></span>
+            <div class="relative h-full p-5 bg-white border-2 border-indigo-500 rounded-lg">
+              <div class="flex items-center -mt-1">
+                <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">DAPP Development</h3>
+              </div>
+              <p class="mt-3 mb-1 text-xs font-medium text-indigo-500 uppercase">------------</p>
+              <p class="mb-2 text-gray-600">A decentralized application (dapp) is an application built on a
+                decentralized network that combines a smart contract and a frontend user interface.</p>
+            </div>
+          </div>
+        </div>
+        <div class="w-full sm:w-1/2">
+          <div class="relative h-full ml-0 md:mr-10">
+            <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-purple-500 rounded-lg"></span>
+            <div class="relative h-full p-5 bg-white border-2 border-purple-500 rounded-lg">
+              <div class="flex items-center -mt-1">
+                <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">Web 3.0 Development</h3>
+              </div>
+              <p class="mt-3 mb-1 text-xs font-medium text-purple-500 uppercase">------------</p>
+              <p class="mb-2 text-gray-600">Web 3.0 is the third generation of Internet services that will
+                focus on understanding and analyzing data to provide a semantic web.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col w-full mb-5 sm:flex-row">
+        <div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+          <div class="relative h-full ml-0 mr-0 sm:mr-10">
+            <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-blue-400 rounded-lg"></span>
+            <div class="relative h-full p-5 bg-white border-2 border-blue-400 rounded-lg">
+              <div class="flex items-center -mt-1">
+                <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">Project Audit</h3>
+              </div>
+              <p class="mt-3 mb-1 text-xs font-medium text-blue-400 uppercase">------------</p>
+              <p class="mb-2 text-gray-600">A Project Audit is a formal review of a project, which is intended
+                to assess the extent up to which project management standards are being upheld.</p>
+            </div>
+          </div>
+        </div>
+        <div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+          <div class="relative h-full ml-0 mr-0 sm:mr-10">
+            <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-yellow-400 rounded-lg"></span>
+            <div class="relative h-full p-5 bg-white border-2 border-yellow-400 rounded-lg">
+              <div class="flex items-center -mt-1">
+                <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">Hacking / RE</h3>
+              </div>
+              <p class="mt-3 mb-1 text-xs font-medium text-yellow-400 uppercase">------------</p>
+              <p class="mb-2 text-gray-600">A security hacker is someone who explores methods for breaching
+                defenses and exploiting weaknesses in a computer system or network.</p>
+            </div>
+          </div>
+        </div>
+        <div class="w-full sm:w-1/2">
+          <div class="relative h-full ml-0 md:mr-10">
+            <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-green-500 rounded-lg"></span>
+            <div class="relative h-full p-5 bg-white border-2 border-green-500 rounded-lg">
+              <div class="flex items-center -mt-1">
+                <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">Bot/Script Development</h3>
+              </div>
+              <p class="mt-3 mb-1 text-xs font-medium text-green-500 uppercase">------------</p>
+              <p class="mb-2 text-gray-600">Bot development frameworks were created as advanced software tools
+                that eliminate a large amount of manual work and accelerate the development process.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
