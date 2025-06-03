1955a5e58f079a36553c3b78210e356be8e573ce
Folkert Jongbloed <f.jongbloed@cowmanager.com>
2025-06-03 13:06:56 +0200

    new panel
    Adds new dashboard panel component
    
    Adds a new reusable dashboard panel component.
    
    This panel provides a structured layout for displaying
    information, including a title, subtitle, and relevant metadata.

---
+++ components/DashboardPanel.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col items-center gap-6 p-7  rounded-2xl">
+    <div>
+      <div class="rounded-lg bg-green-100 p-5">Panel</div>
+    </div>
+    <div class="flex items-center">
+      <span class="text-2xl font-medium">Class Warfare</span>
+      <span class="font-medium text-sky-500">The Anti-Patterns</span>
+      <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+        <span>No. 4</span>
+        <span>·</span>
+        <span>2025</span>
+      </span>
+    </div>
+  </div>
+</template>
