50ab601c59d614900612b3a843ad9761a5210755
Folkert Jongbloed <f.jongbloed@cowmanager.com>
2025-06-03 12:53:21 +0200

    Updates panel background color
    Updates the background color of the panels in the dashboard from indigo to green to improve visual aesthetics.

--- components/CowComponent.vue
+++ components/CowComponent.vue
@@ -1,11 +1,11 @@
 <template>
   <h1>Dashboard</h1>
   <div class="grid grid-cols-3 gap-2">
-    <div class="rounded-lg bg-indigo-100 p-5">Panel</div>
-    <div class="rounded-lg bg-indigo-100 p-5">Panel</div>
-    <div class="rounded-lg bg-indigo-100 p-5">Panel</div>
-    <div class="rounded-lg bg-indigo-100 p-5">Panel</div>
-    <div class="rounded-lg bg-indigo-100 p-5">Panel</div>
-    <div class="rounded-lg bg-indigo-100 p-5">Panel</div>
+    <div class="rounded-lg bg-green-100 p-5">Panel</div>
+    <div class="rounded-lg bg-green-100 p-5">Panel</div>
+    <div class="rounded-lg bg-green-100 p-5">Panel</div>
+    <div class="rounded-lg bg-green-100 p-5">Panel</div>
+    <div class="rounded-lg bg-green-100 p-5">Panel</div>
+    <div class="rounded-lg bg-green-100 p-5">Panel</div>
   </div>
 </template>
